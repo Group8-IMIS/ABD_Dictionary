9b2871f47f7f3af8cc069a36455e01f839e0d08c
Wang Junbo <cr9294wjb@163.com>
2023-09-30 00:01:54 +0800

    增加错误单词查询，相似单词的重新选择

--- code/search.py
+++ code/search.py
@@ -39,13 +39,24 @@ class Dictionary:
             return f"Word: {unknown_word}\nTranslation: {translation}"
         else:
             # 如果没有找到精确匹配，尝试模糊匹配
-            close_matches = difflib.get_close_matches(word_to_lookup, [entry['word'] for entry in self.dictionary], n=3, cutoff=0.6)
-
+            close_matches = difflib.get_close_matches(word_to_lookup, [entry['word'] for entry in self.dictionary], n=5,
+                                                      cutoff=0.6)
             if close_matches:
                 suggestions = "\nDid you mean one of these?\n" + "\n".join(close_matches)
-                return f"{unknown_word} not found. {suggestions}"
+                user_choice = input(
+                    f"{unknown_word} not found. {suggestions}\nPlease select a suggestion or enter another word: ")
+                if user_choice in close_matches:
+                    selected_word = user_choice
+                    selected_translation = self.binary_search(selected_word)
+                    return f"Word: {selected_word}\nTranslation: {selected_translation}"
+                else:
+                    return f"Invalid choice. {unknown_word} not found in the dictionary."
             else:
                 return f"{unknown_word} not found in the dictionary."
+
+
+
+
 
 # 创建 Dictionary 实例
 my_dictionary = Dictionary('EnWords.csv')
